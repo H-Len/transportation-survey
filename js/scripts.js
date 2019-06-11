c2b8f877001c688b33f0d268620a39271dd17afa
Hannah Melendy and Mike Lofgren <hannah@>
2019-06-11 14:37:15 -0700

    add new form for recreational transportation modes.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,17 @@ $(document).ready(function() {
   $("form#transportation_survey").submit(function(event) {
     event.preventDefault();
     $("#work-responses").show();
+    $("#rec-responses").show()
     $("input:checkbox[name=work-transportation]:checked").each(function() {
       var workTransportationMode = $(this).val();
       $("#work-responses").append(workTransportationMode + "<br>");
     });
+    $("input:checkbox[name=recreational-transit]:checked").each(function() {
+      var recTransitMode = $(this).val();
+      $("#rec-responses").append(recTransitMode + "<br>");
+
+    });
+
     $("#transportation_survey").hide();
   });
 });
